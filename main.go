86c3966fbdd00e16974f82f57cc92d5fb40f8de7
Wisnu Anggoro <wisnu@anggoro.net>
2021-03-07 17:22:28 +0100

    Move router to package

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 	"strings"
@@ -9,7 +10,9 @@ import (
 
 	"github.com/mtslzr/pokeapi-go"
 	"github.com/wisnuanggoro/pokedex-web-go/config"
+	"github.com/wisnuanggoro/pokedex-web-go/handlers"
 	"github.com/wisnuanggoro/pokedex-web-go/utils/render"
+	"github.com/wisnuanggoro/pokedex-web-go/utils/router"
 )
 
 func main() {
@@ -26,8 +29,21 @@ func main() {
 	// Initialize utilities
 	renderUtil := render.NewRender(&cfg)
 
+	// Create template cache
+	templateCache, err := renderUtil.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache")
+	}
+	cfg.TemplateCache = templateCache
+
+	// Initialize handlers
+	errorHandler := handlers.NewErrorHandler(renderUtil)
+	homeHandler := handlers.NewHomeHandler(cfg, renderUtil, errorHandler)
+	detailHandler := handlers.NewDetailHandler(renderUtil, errorHandler)
+
 	// Initialize Router
-	router := InitRouter(renderUtil, &cfg)
+	routerUtil := router.NewRouter(errorHandler, homeHandler, detailHandler)
+	router := routerUtil.InitRouter(renderUtil, &cfg)
 
 	// Run server
 	fmt.Println(fmt.Sprintf("Starting application on port %s", cfg.Port))
---
+++ utils/router/router.go
@@ -0,0 +1,49 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/wisnuanggoro/pokedex-web-go/config"
+	"github.com/wisnuanggoro/pokedex-web-go/handlers"
+	"github.com/wisnuanggoro/pokedex-web-go/utils/render"
+)
+
+type router struct {
+	errorHandler  handlers.ErrorHandler
+	homeHandler   handlers.HomeHandler
+	detailHandler handlers.DetailHandler
+}
+
+type Router interface {
+	InitRouter(render render.Render, cfg *config.Config) *mux.Router
+}
+
+func NewRouter(
+	errorHandler handlers.ErrorHandler,
+	homeHandler handlers.HomeHandler,
+	detailHandler handlers.DetailHandler) Router {
+	return &router{
+		errorHandler:  errorHandler,
+		homeHandler:   homeHandler,
+		detailHandler: detailHandler,
+	}
+}
+
+func (rtr *router) InitRouter(render render.Render, cfg *config.Config) *mux.Router {
+
+	// Initialize router
+	r := mux.NewRouter()
+	r.HandleFunc("/", rtr.homeHandler.HomePage).Methods("GET")
+	r.HandleFunc("/detail/{id}", rtr.detailHandler.DetailPage).Methods("GET")
+
+	// Initialize static folder
+	fsImage := http.FileServer(http.Dir("./views/assets/img"))
+	r.PathPrefix("/assets/img/").Handler(http.StripPrefix("/assets/img/", fsImage))
+	fsCSS := http.FileServer(http.Dir("./views/assets/css"))
+	r.PathPrefix("/assets/css/").Handler(http.StripPrefix("/assets/css/", fsCSS))
+	fsJs := http.FileServer(http.Dir("./views/assets/js"))
+	r.PathPrefix("/assets/js/").Handler(http.StripPrefix("/assets/js/", fsJs))
+
+	return r
+}
